d4bb74f6947961dcbe419291d5075f60e7a07142
= <miradorzander@gmail.com>
2022-01-20 23:00:00 +0800

    linis

--- client/src/pages-landing/Landing.js
+++ client/src/pages-landing/Landing.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import {
   BrowserRouter as Router,
   Redirect,
@@ -10,15 +11,40 @@ import {
 //importing components
 import NavbarLandingpage from '../components/Navbar_landingpage';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Poppins',
+    color: '#3F3D56',
+  },
+  pallete: {
+    primary: {
+      main: '#3F3D56',
+    },
+  },
+  mixins: {
+    toolbar: {
+      minHeight: '56px',
+      '@media (min-width: 0px) and (orientation:landscape)': {
+        minHeight: '50px',
+      },
+      '@media (min-width: 600px)': {
+        minHeight: '64px',
+      },
+    },
+  },
+});
+
 function Landing() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path={'/:page?'}>
-          <NavbarLandingpage />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route exact path={'/:page?'}>
+            <NavbarLandingpage />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
