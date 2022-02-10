64cd823f781c8b16d3a0eea9d3a7c8ee34a04068
= <miradorzander@gmail.com>
2022-02-10 20:16:22 +0800

    byms

--- client/src/Telecon/Telecon.jsx
+++ client/src/Telecon/Telecon.jsx
@@ -1,17 +1,96 @@
+// import React from 'react';
+// import Actions from './components/Actions';
+// import ActionPanel from './components/ActionPanel';
+// import VideoPanel from './components/VideoPanel';
+
+// function Telecon() {
+
+//     return (<div>
+//         <Actions />
+//         <div style={{ display: 'flex' }}><ActionPanel />
+//             <VideoPanel /></div>
+
+//     </div>);
+// }
+
+// export default Telecon;
+
 import React from 'react';
-import Actions from './components/Actions';
-import ActionPanel from './components/ActionPanel';
-import VideoPanel from './components/VideoPanel';
+import {
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  FormControl,
+  Typography,
+  Grid,
+  TextField,
+  Container,
+} from '@mui/material';
 
-function Telecon() {
-    
-    
-    return (<div>
-        <Actions />
-        <div style={{ display: 'flex' }}><ActionPanel />
-            <VideoPanel /></div>
+function Radiobtn() {
+  const [selectcategory, setselectedCategory] = React.useState('automate');
 
-    </div>);
+  const handleChange = (event) => {
+    setselectedCategory(event.target.value);
+  };
+
+  return (
+    <Container maxWidth="lg">
+      <div>
+        <FormControl sx={{ display: 'flex' }}>
+          <Typography variant="h5"> Make Groups!</Typography>
+          <RadioGroup
+            sx={{
+              m: 'auto 0 auto auto',
+              display: 'flex',
+              flexDirection: 'row',
+              justifySelf: 'flex-end',
+              alignItems: 'center',
+            }}
+            value={selectcategory}
+            onChange={handleChange}
+          >
+            <FormControlLabel
+              value="automate"
+              control={<Radio />}
+              label="Automate"
+            />
+            <FormControlLabel
+              value="manual"
+              control={<Radio />}
+              label="Manual"
+            />
+            <FormControlLabel
+              value="savedgroups"
+              control={<Radio />}
+              label="Saved Groups"
+            />
+          </RadioGroup>
+        </FormControl>
+      </div>
+
+      <div
+        sx={{
+          display: 'flex',
+          justifyContent: ' center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography> Allocate 5 students into</Typography>
+        <TextField
+          sx={{ width: ' 150px', fontWeight: 'bolder' }}
+          size="small"
+          id="outlined-number"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+        />
+
+        <Typography>Groups</Typography>
+      </div>
+    </Container>
+  );
 }
 
-export default Telecon;
+export default Radiobtn;
