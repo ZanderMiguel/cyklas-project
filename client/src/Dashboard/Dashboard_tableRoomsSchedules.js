5c4d9cce8738f58097b8b22bdd9d0f71b7cd0231
= <miradorzander@gmail.com>
2022-04-13 22:45:14 +0800

    grading

--- client/src/Dashboard/Dashboard_tableRoomsSchedules.js
+++ client/src/Dashboard/Dashboard_tableRoomsSchedules.js
@@ -1,145 +1,144 @@
-import React from 'react'
-import "./Styles/Dashboard_tableRoomsSchedules_stylesheet.css";
+import React from 'react';
+import './Styles/Dashboard_tableRoomsSchedules_stylesheet.css';
 
 const dataTable = [
-    {
-        roomId: "0",
-        roomName: "Embedded Programming",
-        roomCourse: "BSCS",
-        roomYearSec: "3A",
-        classDay: "Saturday",
-        classTime: "10:00 AM",
-        gradingSystem: "Default"
-    },
-    {
-        roomId: "0",
-        roomName: "Embedded Programming",
-        roomCourse: "BSCS",
-        roomYearSec: "3A",
-        classDay: "Saturday",
-        classTime: "10:00 AM",
-        gradingSystem: "Default"
-    },
-    {
-        roomId: "0",
-        roomName: "Embedded Programming",
-        roomCourse: "BSCS",
-        roomYearSec: "3A",
-        classDay: "Saturday",
-        classTime: "10:00 AM",
-        gradingSystem: "Default"
-    },
-    {
-        roomId: "0",
-        roomName: "Embedded Programming",
-        roomCourse: "BSCS",
-        roomYearSec: "3A",
-        classDay: "Saturday",
-        classTime: "10:00 AM",
-        gradingSystem: "Default"
-    },
-    {
-        roomId: "0",
-        roomName: "Embedded Programming",
-        roomCourse: "BSCS",
-        roomYearSec: "3A",
-        classDay: "Saturday",
-        classTime: "10:00 AM",
-        gradingSystem: "Default"
-    },
-    {
-        roomId: "0",
-        roomName: "Embedded Programming",
-        roomCourse: "BSCS",
-        roomYearSec: "3A",
-        classDay: "Saturday",
-        classTime: "10:00 AM",
-        gradingSystem: "Default"
-    },
-    {
-        roomId: "0",
-        roomName: "Embedded Programming",
-        roomCourse: "BSCS",
-        roomYearSec: "3A",
-        classDay: "Saturday",
-        classTime: "10:00 AM",
-        gradingSystem: "Default"
-    },
-    {
-        roomId: "0",
-        roomName: "Embedded Programming",
-        roomCourse: "BSCS",
-        roomYearSec: "3A",
-        classDay: "Saturday",
-        classTime: "10:00 AM",
-        gradingSystem: "Default"
-    },
-    {
-        roomId: "0",
-        roomName: "Embedded Programming",
-        roomCourse: "BSCS",
-        roomYearSec: "3A",
-        classDay: "Saturday",
-        classTime: "10:00 AM",
-        gradingSystem: "Default"
-    },
-    {
-        roomId: "0",
-        roomName: "Embedded Programming",
-        roomCourse: "BSCS",
-        roomYearSec: "3A",
-        classDay: "Saturday",
-        classTime: "10:00 AM",
-        gradingSystem: "Default"
-    }
+  {
+    roomId: '0',
+    roomName: 'Embedded Programming',
+    roomCourse: 'BSCS',
+    roomYearSec: '3A',
+    classDay: 'Saturday',
+    classTime: '10:00 AM',
+    gradingSystem: 'Default',
+  },
+  {
+    roomId: '0',
+    roomName: 'Embedded Programming',
+    roomCourse: 'BSCS',
+    roomYearSec: '3A',
+    classDay: 'Saturday',
+    classTime: '10:00 AM',
+    gradingSystem: 'Default',
+  },
+  {
+    roomId: '0',
+    roomName: 'Embedded Programming',
+    roomCourse: 'BSCS',
+    roomYearSec: '3A',
+    classDay: 'Saturday',
+    classTime: '10:00 AM',
+    gradingSystem: 'Default',
+  },
+  {
+    roomId: '0',
+    roomName: 'Embedded Programming',
+    roomCourse: 'BSCS',
+    roomYearSec: '3A',
+    classDay: 'Saturday',
+    classTime: '10:00 AM',
+    gradingSystem: 'Default',
+  },
+  {
+    roomId: '0',
+    roomName: 'Embedded Programming',
+    roomCourse: 'BSCS',
+    roomYearSec: '3A',
+    classDay: 'Saturday',
+    classTime: '10:00 AM',
+    gradingSystem: 'Default',
+  },
+  {
+    roomId: '0',
+    roomName: 'Embedded Programming',
+    roomCourse: 'BSCS',
+    roomYearSec: '3A',
+    classDay: 'Saturday',
+    classTime: '10:00 AM',
+    gradingSystem: 'Default',
+  },
+  {
+    roomId: '0',
+    roomName: 'Embedded Programming',
+    roomCourse: 'BSCS',
+    roomYearSec: '3A',
+    classDay: 'Saturday',
+    classTime: '10:00 AM',
+    gradingSystem: 'Default',
+  },
+  {
+    roomId: '0',
+    roomName: 'Embedded Programming',
+    roomCourse: 'BSCS',
+    roomYearSec: '3A',
+    classDay: 'Saturday',
+    classTime: '10:00 AM',
+    gradingSystem: 'Default',
+  },
+  {
+    roomId: '0',
+    roomName: 'Embedded Programming',
+    roomCourse: 'BSCS',
+    roomYearSec: '3A',
+    classDay: 'Saturday',
+    classTime: '10:00 AM',
+    gradingSystem: 'Default',
+  },
+  {
+    roomId: '0',
+    roomName: 'Embedded Programming',
+    roomCourse: 'BSCS',
+    roomYearSec: '3A',
+    classDay: 'Saturday',
+    classTime: '10:00 AM',
+    gradingSystem: 'Default',
+  },
 ];
 
 function Dashboard_tableRoomsSchedules() {
   return (
     <div>
-        <div class = "header">
-            <h1> Rooms and Classes Schedules </h1>
-            <div class = "flexgrow-box"></div>
-            <button class = "viewall-btn"> View All </button> 
-        </div>
-        
-        <table class = "rooms-table" >
-            <thead>
-              <tr>
-                <th> Room ID </th>
-                <th> Room Name </th>
-                <th> Course </th>
-                <th> Year and Section </th>
-                <th> Class Day </th>
-                <th> Class Time </th>
-                <th> Grading System </th>
-                <th> Actions </th>
-              </tr>
-            </thead>
+      <div className="header">
+        <h1> Rooms and Classes Schedules </h1>
+        <div className="flexgrow-box"></div>
+        <button className="viewall-btn"> View All </button>
+      </div>
 
-            <tbody>
-            {dataTable.map(function(items, index) {
-                return (
-                <tr key = {index}>
-                    <td data-label = "Room ID"> {items.roomId} </td>
-                    <td data-label = "Room Name"> {items.roomName} </td>
-                    <td data-label = "Course"> {items.roomCourse} </td>
-                    <td data-label = "Year and Section"> {items.roomYearSec} </td>
-                    <td data-label = "Class Day"> {items.classDay} </td>
-                    <td data-label = "Class Time"> {items.classTime} </td>
-                    <td data-label = "Grading System"> {items.gradingSystem} </td>
-                    <td data-label = ""> 
-                        <button class = "edit-btn"> View </button> 
-                        <button class = "leave-btn"> Leave </button> 
-                    </td>
-                </tr>
-                )
-            })}
-              
-            </tbody>
-        </table>
+      <table className="rooms-table">
+        <thead>
+          <tr>
+            <th> Room ID </th>
+            <th> Room Name </th>
+            <th> Course </th>
+            <th> Year and Section </th>
+            <th> Class Day </th>
+            <th> Class Time </th>
+            <th> Grading System </th>
+            <th> Actions </th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {dataTable.map(function (items, index) {
+            return (
+              <tr key={index}>
+                <td data-label="Room ID"> {items.roomId} </td>
+                <td data-label="Room Name"> {items.roomName} </td>
+                <td data-label="Course"> {items.roomCourse} </td>
+                <td data-label="Year and Section"> {items.roomYearSec} </td>
+                <td data-label="Class Day"> {items.classDay} </td>
+                <td data-label="Class Time"> {items.classTime} </td>
+                <td data-label="Grading System"> {items.gradingSystem} </td>
+                <td data-label="">
+                  <button className="edit-btn"> View </button>
+                  <button className="leave-btn"> Leave </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default Dashboard_tableRoomsSchedules
+export default Dashboard_tableRoomsSchedules;
--- client/src/Records/Grading_system.js
+++ client/src/Records/Grading_system.js
@@ -1,9 +1,155 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  Grid,
+  Typography,
+  Button,
+  Box,
+  Paper,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  AccordionActions,
+  Divider,
+  TextField,
+  IconButton,
+} from '@mui/material';
+import { ExpandMore, Remove, Add } from '@mui/icons-material';
 
 function Grading_system() {
+  const [expanded, setExpanded] = useState(false);
+  const [input, setInput] = useState([{ category: '', percentage: '' }]);
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  const handleChangeInput = (index, event) => {
+    const values = [...input];
+    values[index][event.target.name] = event.target.value;
+    setInput(values);
+  };
+
+  const handleAddFields = () => {
+    setInput([...input, { cateory: '', percentage: '' }]);
+  };
+  const handleRemoveFields = (index) => {
+    const values = [...input];
+    values.splice(index, 1);
+    setInput(values);
+  };
+  console.log(input);
+
   return (
-    <div>Grading_system</div>
-  )
+    <>
+      <Grid container>
+        <Box width="100%" display="flex" marginBottom={2}>
+          <Box flexGrow={1} />
+          <Box>
+            <Button variant="contained">Add Grading</Button>
+          </Box>
+        </Box>
+        <Grid item xs={12}>
+          <Accordion
+            expanded={expanded === 'panel1'}
+            onChange={handleChange('panel1')}
+          >
+            <AccordionSummary
+              expandIcon={<ExpandMore />}
+              aria-controls="panel1bh-content"
+              id="panel1bh-header"
+            >
+              <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                General settings
+              </Typography>
+            </AccordionSummary>
+            <Divider />
+            <AccordionDetails>
+              <Box display="flex" sx={{ mb: 2 }}>
+                <Box flexGrow={1}>
+                  <Typography variant="h6"> Class Standing</Typography>
+                </Box>
+                <Box>
+                  <Typography>60%</Typography>
+                </Box>
+              </Box>
+              {input.map((item, index) => (
+                <Box key={index} sx={{ mb: 2 }}>
+                  <TextField
+                    name="category"
+                    label="Category"
+                    value={item.category}
+                    variant="outlined"
+                    sx={{ mr: 2 }}
+                    onChange={(event) => handleChangeInput(index, event)}
+                  />
+                  <TextField
+                    name="percentage"
+                    label="Percentage"
+                    value={item.percentage}
+                    variant="outlined"
+                    onChange={(event) => handleChangeInput(index, event)}
+                  />
+                  {index > 0 && (
+                    <IconButton
+                      sx={{ ml: 2 }}
+                      onClick={() => handleRemoveFields(index)}
+                    >
+                      <Remove />
+                    </IconButton>
+                  )}
+                  <IconButton sx={{ ml: 2 }} onClick={() => handleAddFields()}>
+                    <Add />
+                  </IconButton>
+                </Box>
+              ))}
+              <Box display="flex" sx={{ mb: 2 }}>
+                <Box flexGrow={1}>
+                  <Typography variant="h5"> Class Standing</Typography>
+                </Box>
+                <Box>
+                  <Typography>60%</Typography>
+                </Box>
+              </Box>
+              {input.map((item, index) => (
+                <Box key={index} sx={{ mb: 2 }}>
+                  <TextField
+                    name="category"
+                    label="Category"
+                    value={item.category}
+                    variant="outlined"
+                    sx={{ mr: 2 }}
+                    onChange={(event) => handleChangeInput(index, event)}
+                  />
+                  <TextField
+                    name="percentage"
+                    label="Percentage"
+                    value={item.percentage}
+                    variant="outlined"
+                    onChange={(event) => handleChangeInput(index, event)}
+                  />
+                  {index > 0 && (
+                    <IconButton
+                      sx={{ ml: 2 }}
+                      onClick={() => handleRemoveFields(index)}
+                    >
+                      <Remove />
+                    </IconButton>
+                  )}
+                  <IconButton sx={{ ml: 2 }} onClick={() => handleAddFields()}>
+                    <Add />
+                  </IconButton>
+                </Box>
+              ))}
+            </AccordionDetails>
+            <Divider />
+            <AccordionActions>
+              <Button variant="text">delete</Button>
+              <Button variant="contained"> save</Button>
+            </AccordionActions>
+          </Accordion>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
-export default Grading_system
+export default Grading_system;
