7db866160fff712436ad00d882b99ca9b4d91181
= <miradorzander@gmail.com>
2022-04-22 00:25:09 +0800

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -61,7 +61,7 @@ import ROOMS_INSIDE2 from './student_side/Rooms/Rooms_Inside2';
 import Rooms_main from './student_side/Rooms/Rooms_main';
 import Settings_main from './student_side/Settings/Settings_main';
 import axios from 'axios';
-import Room from './components/Room'
+
 function App() {
   const theme = createTheme({
     typography: {
@@ -100,139 +100,158 @@ function App() {
       />
     );
   });
-  axios.post('http://localhost:5000/rooms')
+  axios.post('http://localhost:5000/rooms');
   return (
     <>
-      {<ThemeProvider theme={theme}>
-        <Router>
-          <Switch>
-            <Redirect exact from="/" to="/home/login" />
-            <Redirect exact from="/home" to="/home/login" />
-            <Route path="/forgotpassword" component={Forgot_password} />
-            <Route exact path="/home/:page?" component={Navbar_landingpage} />
-            <ProtectedRoutes exact path="/dashboard" component={Dashboard} />
-            <ProtectedRoutes exact path="/telecon" component={TeleconStart} />
-            <ProtectedRoutes
-              exact
-              path="/rooms"
-              component={Rooms}
-              socket={socket}
-            />
-            {JSON.parse(localStorage.userData).data.user.userType === "Professor" ? <ProtectedRoutes
-              exact
-              path="/rooms/:roomID"
-              socket={socket}
-              component={Room_inside}
-            /> : <ProtectedRoutes
-              exact
-              path="/rooms/:roomID"
-              socket={socket}
-              component={ROOMS_INSIDE2}
-            />}
-            <ProtectedRoutes
-              exact
-              path="/rooms/:roomID/:activityID"
-              component={View_activity}
-            />
-            <ProtectedRoutes exact path="/settings" component={Setting} />
-            <ProtectedRoutes exact path="/quizlit" component={QuizLit} />
-            <ProtectedRoutes
-              exact
-              path="/quizlit/createquiz"
-              component={Quizform}
-            />
-            <ProtectedRoutes
-              exact
-              path="/quizlit/createexam"
-              component={Examform}
-            />
-            {/* Javen Routes */}
-            {quizlit}
-            <ProtectedRoutes
-              exact
-              path="/StudentLiveQuiz_multiplechoice"
-              component={StudentLiveQuiz_multiplechoice}
-            />
-            <ProtectedRoutes
-              exact
-              path="/Livequiz_correctanswer"
-              component={Livequiz_correctanswer}
-              socket={socket}
-            />
-            <ProtectedRoutes
-              exact
-              path="/Livequiz_wronganswer"
-              component={Livequiz_wronganswer}
-              socket={socket}
-            />
-            <ProtectedRoutes
-              exact
-              path="/student_rankings"
-              component={Student_rankings}
-              socket={socket}
-            />
-            <Route
-              exact
-              path="/Student_activities"
-              component={Student_activities}
-            />
-            <Route
-              exact
-              path="/Student_viewactivity"
-              component={Student_viewactivity}
-            />
-            <Route
-              exact
-              path="/Student_viewexam"
-              component={Student_viewexam}
-            />
-            <Route
-              exact
-              path="/Student_viewquiz"
-              component={Student_viewquiz}
-            />
-            <Route
-              exact
-              path="/Notifications_viewall"
-              component={Notifications_viewall}
-            />
+      {
+        <ThemeProvider theme={theme}>
+          <Router>
+            <Switch>
+              <Redirect exact from="/" to="/home/login" />
+              <Redirect exact from="/home" to="/home/login" />
+              <Route path="/forgotpassword" component={Forgot_password} />
+              <Route exact path="/home/:page?" component={Navbar_landingpage} />
+              <ProtectedRoutes exact path="/dashboard" component={Dashboard} />
+              <ProtectedRoutes exact path="/telecon" component={TeleconStart} />
+              <ProtectedRoutes
+                exact
+                path="/rooms"
+                component={Rooms}
+                socket={socket}
+              />
+              {JSON.parse(localStorage.userData).data.user.userType ===
+              'Professor' ? (
+                <ProtectedRoutes
+                  exact
+                  path="/rooms/:roomID"
+                  socket={socket}
+                  component={Room_inside}
+                />
+              ) : (
+                <ProtectedRoutes
+                  exact
+                  path="/rooms/:roomID"
+                  socket={socket}
+                  component={Room_inside}
+                />
+              )}
+              <ProtectedRoutes
+                exact
+                path="/rooms/:roomID/:activityID"
+                component={View_activity}
+              />
+              <ProtectedRoutes exact path="/settings" component={Setting} />
+              <ProtectedRoutes exact path="/quizlit" component={QuizLit} />
+              <ProtectedRoutes
+                exact
+                path="/quizlit/createquiz"
+                component={Quizform}
+              />
+              <ProtectedRoutes
+                exact
+                path="/quizlit/createexam"
+                component={Examform}
+              />
+              {/* Javen Routes */}
+              {quizlit}
+              <ProtectedRoutes
+                exact
+                path="/StudentLiveQuiz_multiplechoice"
+                component={StudentLiveQuiz_multiplechoice}
+              />
+              <ProtectedRoutes
+                exact
+                path="/Livequiz_correctanswer"
+                component={Livequiz_correctanswer}
+                socket={socket}
+              />
+              <ProtectedRoutes
+                exact
+                path="/Livequiz_wronganswer"
+                component={Livequiz_wronganswer}
+                socket={socket}
+              />
+              <ProtectedRoutes
+                exact
+                path="/student_rankings"
+                component={Student_rankings}
+                socket={socket}
+              />
+              <Route
+                exact
+                path="/Student_activities"
+                component={Student_activities}
+              />
+              <Route
+                exact
+                path="/Student_viewactivity"
+                component={Student_viewactivity}
+              />
+              <Route
+                exact
+                path="/Student_viewexam"
+                component={Student_viewexam}
+              />
+              <Route
+                exact
+                path="/Student_viewquiz"
+                component={Student_viewquiz}
+              />
+              <Route
+                exact
+                path="/Notifications_viewall"
+                component={Notifications_viewall}
+              />
 
-            {/* Student Side */}
-            <Route exact path="/ClassCards_main" component={ClassCards_main} />
-            <Route exact path="/Evaluation" component={Evaluation} />
-            <Route exact path="/Dashboard_main" component={Dashboard_main} />
-            <Route exact path="/Activities_main" component={Activities_main} />
-            <Route exact path="/Activity_viewed" component={Activity_viewed} />
-            <Route exact path="/Quiz_viewed" component={Quiz_viewed} />
-            <Route exact path="/Exam_viewed" component={Exam_viewed} />
+              {/* Student Side */}
+              <Route
+                exact
+                path="/ClassCards_main"
+                component={ClassCards_main}
+              />
+              <Route exact path="/Evaluation" component={Evaluation} />
+              <Route exact path="/Dashboard_main" component={Dashboard_main} />
+              <Route
+                exact
+                path="/Activities_main"
+                component={Activities_main}
+              />
+              <Route
+                exact
+                path="/Activity_viewed"
+                component={Activity_viewed}
+              />
+              <Route exact path="/Quiz_viewed" component={Quiz_viewed} />
+              <Route exact path="/Exam_viewed" component={Exam_viewed} />
 
-            <Route
-              exact
-              path="/Rooms_main"
-              component={Rooms_main}
-              socket={socket}
-            />
-            <Route exact path="/Settings_main" component={Settings_main} />
+              <Route
+                exact
+                path="/Rooms_main"
+                component={Rooms_main}
+                socket={socket}
+              />
+              <Route exact path="/Settings_main" component={Settings_main} />
 
-            <Route exact path="/Exam_take/:quizID" component={Exam_take} />
-            <Route exact path="/Exam_start/:examID" component={Exam_start} />
-            <Route exact path="/View_quiz" component={View_quiz} />
-            <Route exact path="/View_exam" component={View_exam} />
-            <ProtectedRoutes exact path="/records" component={Records} />
-            <Route path="/telecon/room" component={Telecon_room} />
-            <Route exact path="/quizlit/join">
-              <JoinQuiz socket={socket} />
-            </Route>
-            <Route exact path="/quizlit/lobby">
-              <Lobby socket={socket} />
-            </Route>
-            <Route exact path="/quizlit/lobby/:lobby/:name/:quizID">
-              <ToLobby socket={socket} />
-            </Route>
-            <Route component={Notfound} />
-          </Switch>
-        </Router>
-      </ThemeProvider>}
+              <Route exact path="/Exam_take/:quizID" component={Exam_take} />
+              <Route exact path="/Exam_start/:examID" component={Exam_start} />
+              <Route exact path="/View_quiz" component={View_quiz} />
+              <Route exact path="/View_exam" component={View_exam} />
+              <ProtectedRoutes exact path="/records" component={Records} />
+              <Route path="/telecon/room" component={Telecon_room} />
+              <Route exact path="/quizlit/join">
+                <JoinQuiz socket={socket} />
+              </Route>
+              <Route exact path="/quizlit/lobby">
+                <Lobby socket={socket} />
+              </Route>
+              <Route exact path="/quizlit/lobby/:lobby/:name/:quizID">
+                <ToLobby socket={socket} />
+              </Route>
+              <Route component={Notfound} />
+            </Switch>
+          </Router>
+        </ThemeProvider>
+      }
     </>
   );
 }
