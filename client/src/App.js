3fbc99d2dd050baa50a5be8b137f9d48cba1d2a5
= <miradorzander@gmail.com>
2022-04-25 15:07:52 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -54,10 +54,7 @@ import Telecon_room from './Telecon/Telecon_room';
 import JoinQuiz from './Quizlit/TestComponents/JoinQuiz';
 import ToLobby from './Quizlit/TestComponents/ToLobby';
 import Lobby from './Quizlit/TestComponents/Lobby';
-<<<<<<< Updated upstream
-=======
 import ROOMS_INSIDE2 from './student_side/Rooms/Rooms_inside2';
->>>>>>> Stashed changes
 import Notfound from './Notfound';
 // import LoadQuizlit from './Quizlit/TestComponents/LoadQuizlit';
 
@@ -109,91 +106,6 @@ function App() {
   axios.post('http://localhost:5000/rooms');
   return (
     <>
-<<<<<<< Updated upstream
-      {<ThemeProvider theme={theme}>
-        <Router>
-          <Switch>
-            <Redirect exact from="/" to="/home/login" />
-            <Redirect exact from="/home" to="/home/login" />
-            <Route path="/forgotpassword" component={Forgot_password} />
-            <Route exact path="/home/:page?" component={Navbar_landingpage} />
-            <ProtectedRoutes exact path="/dashboard" component={Dashboard} />
-            <ProtectedRoutes exact path="/telecon" component={TeleconStart} />
-            <ProtectedRoutes
-              exact
-              path="/rooms"
-              component={Rooms}
-              socket={socket}
-            />
-            
-            <ProtectedRoutes
-              exact
-              path="/rooms/:roomID/:activityID"
-              component={View_activity}
-            />
-            <ProtectedRoutes exact path="/settings" component={Setting} />
-            <ProtectedRoutes exact path="/quizlit" component={QuizLit} />
-            <ProtectedRoutes
-              exact
-              path="/quizlit/createquiz"
-              component={Quizform}
-            />
-            <ProtectedRoutes
-              exact
-              path="/quizlit/createexam"
-              component={Examform}
-            />
-            {/* Javen Routes */}
-            {quizlit}
-            <ProtectedRoutes
-              exact
-              path="/StudentLiveQuiz_multiplechoice"
-              component={StudentLiveQuiz_multiplechoice}
-            />
-            <ProtectedRoutes
-              exact
-              path="/Livequiz_correctanswer"
-              component={Livequiz_correctanswer}
-              socket={socket}
-            />
-            <ProtectedRoutes
-              exact
-              path="/Livequiz_wronganswer"
-              component={Livequiz_wronganswer}
-              socket={socket}
-            />
-            <ProtectedRoutes
-              exact
-              path="/student_rankings"
-              component={Student_rankings}
-              socket={socket}
-            />
-            <Route
-              exact
-              path="/Student_activities"
-              component={Student_activities}
-            />
-            <Route
-              exact
-              path="/Student_viewactivity"
-              component={Student_viewactivity}
-            />
-            <Route
-              exact
-              path="/Student_viewexam"
-              component={Student_viewexam}
-            />
-            <Route
-              exact
-              path="/Student_viewquiz"
-              component={Student_viewquiz}
-            />
-            <Route
-              exact
-              path="/Notifications_viewall"
-              component={Notifications_viewall}
-            />
-=======
       {
         <ThemeProvider theme={theme}>
           <Router>
@@ -226,7 +138,6 @@ function App() {
                   component={ROOMS_INSIDE2}
                 />
               )}
->>>>>>> Stashed changes
 
               <ProtectedRoutes
                 exact
@@ -322,28 +233,6 @@ function App() {
               <Route exact path="/Quiz_viewed" component={Quiz_viewed} />
               <Route exact path="/Exam_viewed" component={Exam_viewed} />
 
-<<<<<<< Updated upstream
-            <Route exact path="/Exam_take/:quizID" component={Exam_take} />
-            <Route exact path="/Exam_start/:examID" component={Exam_start} />
-            <Route exact path="/View_quiz" component={View_quiz} />
-            <Route exact path="/View_exam" component={View_exam} />
-            <ProtectedRoutes exact path="/records" component={Records} />
-            <Route path="/telecon/room" component={Telecon_room} />
-            <Route exact path="/quizlit/join">
-              <JoinQuiz socket={socket} />
-            </Route>
-            <Route exact path="/quizlit/lobby">
-              <Lobby socket={socket} />
-            </Route>
-            <Route exact path="/quizlit/lobby/:lobby/:name/:quizID">
-              <ToLobby socket={socket} />
-            </Route>
-            <ProtectedRoutes socket={socket}/>
-            <Route component={Notfound} />
-          </Switch>
-        </Router>
-      </ThemeProvider>}
-=======
               <Route
                 exact
                 path="/Rooms_main"
@@ -372,7 +261,6 @@ function App() {
           </Router>
         </ThemeProvider>
       }
->>>>>>> Stashed changes
     </>
   );
 }
