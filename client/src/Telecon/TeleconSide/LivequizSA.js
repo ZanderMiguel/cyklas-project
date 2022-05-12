dc302498802d5c678b81054c9cb1886f01703fcf
= <miradorzander@gmail.com>
2022-05-13 06:54:45 +0800

    s

--- client/src/Telecon/TeleconSide/LivequizSA.js
+++ client/src/Telecon/TeleconSide/LivequizSA.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { Box, Grid, Backdrop, Typography } from "@mui/material";
-import QuestionResult from "../QuestionResult";
+import React from 'react';
+import { Box, Grid, Backdrop, Typography } from '@mui/material';
+import QuestionResult from '../QuestionResult';
 
-function LivequizSA() {
-const [open, setOpen] = React.useState(false);
+function LivequizSA({ dataChoices }) {
+  const [open, setOpen] = React.useState(false);
   const handleClose = () => {
     setOpen(false);
   };
@@ -13,26 +13,28 @@ const [open, setOpen] = React.useState(false);
 
   return (
     <>
-    {dataChoices.map(function(items, index) {
+      {dataChoices.map(function (items, index) {
         return (
-            <Box key = {index}
-            sx = {{
-                width: "100%",
-                height: "auto"
-            }}>
+          <Box
+            key={index}
+            sx={{
+              width: '100%',
+              height: 'auto',
+            }}
+          >
             {items.choice}
-            </Box>
-        )
-    })}
-    <Backdrop
-sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-open={open}
-onClick={handleClose}
->
-<QuestionResult/>
-</Backdrop>
-</>
-  )
+          </Box>
+        );
+      })}
+      <Backdrop
+        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={open}
+        onClick={handleClose}
+      >
+        <QuestionResult />
+      </Backdrop>
+    </>
+  );
 }
 
-export default LivequizSA
+export default LivequizSA;
