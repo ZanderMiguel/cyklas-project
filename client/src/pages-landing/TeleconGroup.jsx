ad0429dd14a86607f879c872cfb0d4243eb0da2b
= <miradorzander@gmail.com>
2022-05-15 03:06:58 +0800

    still working on breakout

--- client/src/pages-landing/TeleconGroup.jsx
+++ client/src/pages-landing/TeleconGroup.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect, useParams } from 'react-router-dom';
 function TeleconGroup() {
   const { group } = useParams();
-  //const win = window.open(`/telecon/${group}`);
+  const win = window.open(`/telecon/${group}`, '_self');
 
   return (
     <>
